0f03fe0a690152a4bf9f4b266e1389ded484f8e7
Tobi Omotayo <tobitech@ymail.com>
2023-01-14 15:24:44 +0100

    Initial Commit

---
+++ ClocksExploration/ClocksExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClocksExplorationApp.swift
+//  ClocksExploration
+//
+//  Created by Oluwatobi Omotayo on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClocksExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
